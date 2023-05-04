07a8da93d18d1b4a01c0b9a9c9fea18a95b59021
xiongfeng <xiongfeng@mail.llzby.top>
2023-05-04 14:19:54 +0800

    netty字符串编码器

---
+++ src/main/java/cn/xf/improve/netty/test05_字符串编码器/MyChannelInitializer.java
@@ -0,0 +1,33 @@
+package cn.xf.improve.netty.test05_字符串编码器;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.nio.charset.Charset;
+
+/**
+ * @Description:
+ * @ClassName: MyChannelInitializer
+ * @Author: xiongfeng
+ * @Date: 2023/5/4 10:07
+ * @Version: 1.0
+ */
+public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
+	@Override
+	protected void initChannel(SocketChannel channel) throws Exception {
+
+		//添加解码器
+//		channel.pipeline().addLast(new LineBasedFrameDecoder(1024));
+		//设置字符转换格式
+		channel.pipeline().addLast(new StringDecoder(Charset.forName("GBK")));
+		//new 设置字符串编码器
+		channel.pipeline().addLast(new StringEncoder(Charset.forName("GBK")));
+
+		//在管道中添加自己的接收数据实现方法
+		channel.pipeline().addLast(new MyServerHandler());
+
+	}
+
+}
